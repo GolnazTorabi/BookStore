6c197bf8f8543a226fbc6ee0a1a888fe0fc4d794
golnaz <g.torabi76@gmail.com>
2020-08-08 22:37:28 +0430

    login functionality

--- app/src/main/java/com/book/store/stock/bookstore/pages/authentication/login/LoginFragment.kt
+++ app/src/main/java/com/book/store/stock/bookstore/pages/authentication/login/LoginFragment.kt
@@ -1,33 +1,103 @@
 package com.book.store.stock.bookstore.pages.authentication.login
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.book.store.stock.bookstore.R
+import com.book.store.stock.bookstore.databinding.LoginFragmentBinding
+import javax.inject.Inject
 
 class LoginFragment : Fragment() {
 
-    companion object {
-        fun newInstance() =
-            LoginFragment()
-    }
-
     private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: LoginFragmentBinding
+
+    @Inject
+    lateinit var factory: ViewModelProvider.Factory
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.login_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this, factory).get(LoginViewModel::class.java)
+        checkEditTexts()
+        loginButtonClicked()
+        forgetPassword()
+        register()
+    }
+
+    private fun register() {
+        binding.register.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+
+    private fun forgetPassword() {
+        binding.forgetPass.setOnClickListener {
+            //todo go to forget pass
+        }
+    }
+
+    private fun loginButtonClicked() {
+        if (binding.loginButton.isEnabled) {
+            // api call
+            binding.loginButton.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_dashboard_graph) }
+        }
+    }
+
+    private fun checkEditTexts() {
+        binding.codeEdit.doAfterTextChanged {
+            binding.loginButton.isEnabled = it?.length != 4
+        }
+        binding.usernameEdit.doAfterTextChanged {
+            binding.loginButton.isEnabled = it?.length != 5
+        }
+        binding.passwordEdit.doAfterTextChanged {
+            binding.loginButton.isEnabled = it?.length != 8
+        }
+
+        binding.codeEdit.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                binding.codeEditLayout.isErrorEnabled = false
+            } else {
+                if (binding.codeEdit.length() < 4) {
+                    binding.codeEditLayout.isErrorEnabled = true
+                    binding.codeEditLayout.error = "کد کاربری خود را کامل وارد کنید"
+                }
+            }
+        }
+        binding.usernameEdit.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                binding.codeEditLayout.isErrorEnabled = false
+            } else {
+                if (binding.codeEdit.length() < 4) {
+                    binding.codeEditLayout.isErrorEnabled = true
+                    binding.codeEditLayout.error = "نام کاربری خود را کامل وارد کنید"
+                }
+            }
+        }
+        binding.passwordEdit.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                binding.codeEditLayout.isErrorEnabled = false
+            } else {
+                if (binding.codeEdit.length() < 8) {
+                    binding.codeEditLayout.isErrorEnabled = true
+                    binding.codeEditLayout.error = "پسورد خود را کامل وارد کنید"
+                }
+            }
+        }
     }
 
 }
