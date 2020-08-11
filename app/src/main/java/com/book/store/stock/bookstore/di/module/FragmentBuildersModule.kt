6cb6eb01491a665845ad03d24e3a2a5d76c0d1ce
golnaz <g.torabi76@gmail.com>
2020-08-12 00:50:03 +0430

    add dagger to dashboard

--- app/src/main/java/com/book/store/stock/bookstore/di/module/FragmentBuildersModule.kt
+++ app/src/main/java/com/book/store/stock/bookstore/di/module/FragmentBuildersModule.kt
@@ -3,6 +3,7 @@ package com.book.store.stock.bookstore.di.module
 import com.book.store.stock.bookstore.pages.authentication.login.LoginFragment
 import com.book.store.stock.bookstore.pages.authentication.register.RegisterFragment
 import com.book.store.stock.bookstore.pages.authentication.splash.SplashScreenFragment
+import com.book.store.stock.bookstore.pages.dash_board.filter.DashBoardFilterFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -17,5 +18,11 @@ abstract class FragmentBuildersModule {
 
     @ContributesAndroidInjector
     abstract fun contributeSplashFragment(): SplashScreenFragment
+
+    @ContributesAndroidInjector
+    abstract fun contributeDashBoardFragment(): DashBoardFilterFragment
+
+    @ContributesAndroidInjector
+    abstract fun contributeDashBoardFilterFragment(): DashBoardFilterFragment
 
 }
