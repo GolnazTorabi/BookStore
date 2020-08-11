276283ccd78fcbfa2e7619dfac0acda9909dfec2
golnaz <g.torabi76@gmail.com>
2020-08-12 00:01:57 +0430

    define dashboard_book_list adapter

--- app/src/main/java/com/book/store/stock/bookstore/pages/dash_board/DashBoardAdapter.kt
+++ app/src/main/java/com/book/store/stock/bookstore/pages/dash_board/DashBoardAdapter.kt
@@ -2,7 +2,10 @@ package com.book.store.stock.bookstore.pages.dash_board
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.book.store.stock.bookstore.R
+import com.book.store.stock.bookstore.databinding.BookListItemsBinding
 import com.book.store.stock.bookstore.utility.LoadMoreListener
 
 class DashBoardAdapter(var list: ArrayList<String>,var onLoadMoreListener: LoadMoreListener): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -13,14 +16,27 @@ class DashBoardAdapter(var list: ArrayList<String>,var onLoadMoreListener: LoadM
         if (layoutInflater == null) {
             layoutInflater = LayoutInflater.from(parent.context)
         }
+        val binding: BookListItemsBinding = DataBindingUtil.inflate(layoutInflater!!, R.layout.book_list_items, parent, false)
+        return BookListViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        if (holder is BookListViewHolder){
+            holder.binding.name.text = list[position]
+            holder.binding.translatorName.text = list[position]
+            holder.binding.price.text = list[position]
+            holder.binding.stock.text = list[position]
+            holder.binding.publishYear.text = list[position]
+            holder.binding.publishedTime.text = list[position]
+            holder.binding.publisher.text = list[position]
+        }
+    }
+    class BookListViewHolder(var binding: BookListItemsBinding) : RecyclerView.ViewHolder(binding.root) {
+
     }
 
 }
