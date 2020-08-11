f887107fa6820bc25de8dc8eab7ad80edaaa2731
golnaz <g.torabi76@gmail.com>
2020-08-12 00:10:05 +0430

    create notification items adapter

--- app/src/main/java/com/book/store/stock/bookstore/pages/dash_board/DashBoardNotifAdapter.kt
+++ app/src/main/java/com/book/store/stock/bookstore/pages/dash_board/DashBoardNotifAdapter.kt
@@ -6,9 +6,10 @@ import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.book.store.stock.bookstore.R
 import com.book.store.stock.bookstore.databinding.BookListItemsBinding
+import com.book.store.stock.bookstore.databinding.NotificationListBinding
 import com.book.store.stock.bookstore.utility.LoadMoreListener
 
-class DashBoardAdapter(var list: ArrayList<String>,var onLoadMoreListener: LoadMoreListener): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class DashBoardNotifAdapter (var list: ArrayList<String>,var onLoadMoreListener: LoadMoreListener): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 
     private var layoutInflater: LayoutInflater? = null
@@ -16,8 +17,8 @@ class DashBoardAdapter(var list: ArrayList<String>,var onLoadMoreListener: LoadM
         if (layoutInflater == null) {
             layoutInflater = LayoutInflater.from(parent.context)
         }
-        val binding: BookListItemsBinding = DataBindingUtil.inflate(layoutInflater!!, R.layout.book_list_items, parent, false)
-        return BookListViewHolder(binding)
+        val binding: NotificationListBinding = DataBindingUtil.inflate(layoutInflater!!, R.layout.notification_list, parent, false)
+        return NotificationListViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -25,20 +26,15 @@ class DashBoardAdapter(var list: ArrayList<String>,var onLoadMoreListener: LoadM
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder is BookListViewHolder){
-            holder.binding.name.text = list[position]
-            holder.binding.translatorName.text = list[position]
-            holder.binding.price.text = list[position]
-            holder.binding.stock.text = list[position]
-            holder.binding.publishYear.text = list[position]
-            holder.binding.publishedTime.text = list[position]
-            holder.binding.publisher.text = list[position]
+        if (holder is NotificationListViewHolder){
+            holder.binding.notificationText.text = list[position]
+            holder.binding.notificationTime.text = list[position]
         }
 
         if (position==list.size-1)
             onLoadMoreListener.onLoadMore()
     }
-    class BookListViewHolder(var binding: BookListItemsBinding) : RecyclerView.ViewHolder(binding.root) {
+    class NotificationListViewHolder(var binding: NotificationListBinding) : RecyclerView.ViewHolder(binding.root) {
 
     }
 
