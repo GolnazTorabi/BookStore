167941024eb591a6b9ee010f929dcf8168b2ad66
golnaz <g.torabi76@gmail.com>
2020-08-13 18:14:04 +0430

    initilized binding

--- app/src/main/java/com/book/store/stock/bookstore/pages/setting/request/seller/SettingRequestSellerFragment.kt
+++ app/src/main/java/com/book/store/stock/bookstore/pages/setting/request/seller/SettingRequestSellerFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.book.store.stock.bookstore.R
+import com.book.store.stock.bookstore.databinding.SettingRequestSellerFragmentBinding
 
 class SettingRequestSellerFragment : Fragment() {
 
@@ -16,17 +19,20 @@ class SettingRequestSellerFragment : Fragment() {
     }
 
     private lateinit var viewModel: SettingRequestSellerViewModel
+    private lateinit var binding: SettingRequestSellerFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.setting_request_seller_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.setting_request_seller_fragment, container, false)
+        viewModel = ViewModelProvider(this).get(SettingRequestSellerViewModel::class.java)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SettingRequestSellerViewModel::class.java)
+
         // TODO: Use the ViewModel
     }
 
