913484b03346d64bbc0151d9d333c7530c89866f
golnaz <g.torabi76@gmail.com>
2020-08-14 18:10:14 +0430

    add order stock

--- app/src/main/java/com/book/store/stock/bookstore/pages/order/OrderFragment.kt
+++ app/src/main/java/com/book/store/stock/bookstore/pages/order/OrderFragment.kt
@@ -1,14 +1,19 @@
 package com.book.store.stock.bookstore.pages.order
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.book.store.stock.bookstore.R
+import com.book.store.stock.bookstore.databinding.OrderFragmentBinding
+import com.book.store.stock.bookstore.pages.dash_board.DashBoardViewModel
+import dagger.android.support.DaggerFragment
+import javax.inject.Inject
 
-class OrderFragment : Fragment() {
+class OrderFragment : DaggerFragment() {
 
     companion object {
         fun newInstance() =
@@ -16,18 +21,53 @@ class OrderFragment : Fragment() {
     }
 
     private lateinit var viewModel: OrderViewModel
+    private lateinit var binding: OrderFragmentBinding
+
+    @Inject
+    lateinit var factory: ViewModelProvider.Factory
+
+    private var isSeller: Boolean? = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.order_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.order_fragment, container, false)
+        viewModel = ViewModelProvider(this, factory).get(OrderViewModel::class.java)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(OrderViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        checkIsSeller()
+    }
+
+    private fun search() {
+        binding.search.setOnClickListener {
+            //api call
+        }
+    }
+
+    private fun checkIsSeller() {
+        viewModel.isSeller()
+        observeIsSeller()
+    }
+
+    private fun observeIsSeller() {
+        viewModel.isSeller.observe(viewLifecycleOwner, Observer {
+            it.let {
+                when (it) {
+                    OrderViewModel.SellerStock.Seller -> {
+
+                    }
+                    OrderViewModel.SellerStock.Stock -> {
+                        isSeller = false
+                        search()
+                    }
+                }
+            }
+        })
     }
 
 }
