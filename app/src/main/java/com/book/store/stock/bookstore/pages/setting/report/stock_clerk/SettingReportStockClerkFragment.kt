af6855a2dd4f4c1c5c8f2f845c9e3ca427d18d61
golnaz <g.torabi76@gmail.com>
2020-08-14 17:49:02 +0430

    add view model and binfing

--- app/src/main/java/com/book/store/stock/bookstore/pages/setting/report/stock_clerk/SettingReportStockClerkFragment.kt
+++ app/src/main/java/com/book/store/stock/bookstore/pages/setting/report/stock_clerk/SettingReportStockClerkFragment.kt
@@ -1,12 +1,15 @@
 package com.book.store.stock.bookstore.pages.setting.report.stock_clerk
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.book.store.stock.bookstore.R
+import com.book.store.stock.bookstore.databinding.SettingReportStockClerkFragmentBinding
+import javax.inject.Inject
 
 class SettingReportStockClerkFragment : Fragment() {
 
@@ -16,17 +19,22 @@ class SettingReportStockClerkFragment : Fragment() {
     }
 
     private lateinit var viewModel: SettingReportStockClerkViewModel
+    private lateinit var binding: SettingReportStockClerkFragmentBinding
+
+    @Inject
+    lateinit var factory: ViewModelProvider.Factory
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.setting_report_stock_clerk_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.setting_report_stock_clerk_fragment, container, false)
+        viewModel = ViewModelProvider(this, factory).get(SettingReportStockClerkViewModel::class.java)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SettingReportStockClerkViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
