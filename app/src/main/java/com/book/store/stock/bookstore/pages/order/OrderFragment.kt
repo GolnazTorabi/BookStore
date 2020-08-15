0ef9466629b46d2c1883a44b7bc5b1c46fe0880e
golnaz <g.torabi76@gmail.com>
2020-08-15 18:54:33 +0430

    order seller fragment

--- app/src/main/java/com/book/store/stock/bookstore/pages/order/OrderFragment.kt
+++ app/src/main/java/com/book/store/stock/bookstore/pages/order/OrderFragment.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.book.store.stock.bookstore.R
 import com.book.store.stock.bookstore.databinding.OrderFragmentBinding
-import com.book.store.stock.bookstore.pages.dash_board.DashBoardViewModel
+import com.book.store.stock.bookstore.pages.setting.new_data.book.SettingNewBookAdapter
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
@@ -27,6 +29,9 @@ class OrderFragment : DaggerFragment() {
     lateinit var factory: ViewModelProvider.Factory
 
     private var isSeller: Boolean? = false
+    private var bookData = ArrayList<String>()
+    private lateinit var adapter: OrderAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,7 +44,7 @@ class OrderFragment : DaggerFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
+        binding.back.setOnClickListener { activity?.onBackPressed() }
         checkIsSeller()
     }
 
@@ -60,6 +65,11 @@ class OrderFragment : DaggerFragment() {
                 when (it) {
                     OrderViewModel.SellerStock.Seller -> {
                         binding.stock.visibility = View.GONE
+                        newBook()
+                        getBardCodeData()
+                        cancel()
+                        submit()
+                        initAdapter()
                     }
                     OrderViewModel.SellerStock.Stock -> {
                         binding.stock.visibility = View.VISIBLE
@@ -68,6 +78,33 @@ class OrderFragment : DaggerFragment() {
                     }
                 }
             }
+        })
+    }
+
+    private fun initAdapter() {
+        adapter = OrderAdapter(bookData)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.list.layoutManager = layoutManager
+        binding.list.adapter = adapter
+    }
+
+    private fun submit() {
+        binding.submit.setOnClickListener {
+            //TODO api call
+        }
+    }
+
+    private fun newBook() {
+        binding.newBook.setOnClickListener { findNavController().navigate(R.id.action_orderFragment_to_orderScannerFragment) }
+    }
+
+    private fun cancel() {
+        binding.cancel.setOnClickListener { activity?.onBackPressed() }
+    }
+
+    private fun getBardCodeData() {
+        findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<ArrayList<String>>("scanner_order")?.observe(viewLifecycleOwner, Observer {
+            //scanner data
         })
     }
 
